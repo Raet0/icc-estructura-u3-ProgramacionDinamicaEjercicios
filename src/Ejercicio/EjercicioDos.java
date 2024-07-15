406b095519439b5829f3bcf74f36e1872efc7c8a
rafael <raet0>
2024-07-15 17:44:38 -0500

    Resolucion de dos ejercicios

--- src/Ejercicio/EjercicioDos.java
+++ src/Ejercicio/EjercicioDos.java
@@ -1,27 +1,47 @@
 package Ejercicio;
 
-/*
- * Dados n pares de paréntesis, implementa un algoritmo para generar todas las combinaciones
- * válidas de paréntesis.
- *
- * Ejemplo 1:
- *  Input:
- *    n: 1
- *  Output:
- *    ["()"]
- *
- * Ejemplo 2:
- *  Input:
- *    n: 3
- *  Output:
- *    ["((()))","(()())","(())()","()(())","()()()"]
- *
- * Ecoja la mejor estructura de datos para que sea mas eficiente
- * Se debe usar StringBuilder
+import java.util.ArrayList;
+import java.util.List;
+
+/*  Dados n pares de paréntesis, implementa un algoritmo para generar todas las combinaciones
+válidas de paréntesis.
+ 
+  Ejemplo 1:
+   Input:
+     n: 1
+   Output:
+     ["()"]
+ 
+  Ejemplo 2:
+   Input:
+     n: 3
+   Output:
+     ["((()))","(()())","(())()","()(())","()()()"]
+ 
+  Ecoja la mejor estructura de datos para que sea mas eficiente
+  Se debe usar StringBuilder
  */
 public class EjercicioDos {
+  public List<String> generateParenthesis(int n) {
+      List<String> result = new ArrayList<>();
+      generate(result, new StringBuilder(), 0, 0, n);
+      return result;
+  }
+  private void generate(List<String> result, StringBuilder current, int open, int close, int max) {
+    if (current.length() == max * 2) {
+        result.add(current.toString());
+        return;
+    }
 
-    // List<List<Integer>> subsets(List<Integer> set) {
-
-    // }
+    if (open < max) {
+        current.append("(");
+        generate(result, current, open + 1, close, max);
+        current.deleteCharAt(current.length() - 1);
+    }
+    if (close < open) {
+        current.append(")");
+        generate(result, current, open, close + 1, max);
+        current.deleteCharAt(current.length() - 1);
+    }
+}  
 }
--- src/Ejercicio/EjercicioUno.java
+++ src/Ejercicio/EjercicioUno.java
@@ -3,33 +3,40 @@ package Ejercicio;
 import java.util.ArrayList;
 import java.util.List;
 
-/*
- * Implementa un método para devolver todos los subconjuntos de un arreglo de enteros
- * 
- * Se puede sin recursividad
- * Ejemplo:
- *  Input: [1,2,3]
- *
- *  Output:
- *    [
- *      [1,2,3],
- *      [1,2],
- *      [1,3],
- *      [2,3],
- *      [1],
- *      [2],
- *      [3],
- *      []
- *    ]
- * 
- * El  metodo me debe devolver una lista de listas de enteros. 
- * Se puede usar varios metodos 
- * 
- * Ecoja la mejor estructura de datos para que sea mas eficiente
- */
+/*Implementa un método para devolver todos los subconjuntos de un arreglo de enteros
+
+Se puede sin recursividad
+Ejemplo:
+ Input: [1,2,3]
+ Output:
+   [
+     [1,2,3],
+     [1,2],
+     [1,3],
+     [2,3],
+     [1],
+     [2],
+     [3],
+     []
+   ]
+
+El  metodo me debe devolver una lista de listas de enteros. 
+Se puede usar varios metodos 
+
+Ecoja la mejor estructura de datos para que sea mas eficiente
+*/
 public class EjercicioUno {
-
-    // List<List<Integer>> subsets(List<Integer> set) {
-
-    // }
+  public List<List<Integer>> subsets(List<Integer> set) {
+      List<List<Integer>> result = new ArrayList<>();
+      result.add(new ArrayList<>());
+      for (int i = 0; i < set.size(); i++) {
+          int size = result.size();
+          for (int j = 0; j < size; j++) {
+              List<Integer> subset = new ArrayList<>(result.get(j));
+              subset.add(set.get(i));
+              result.add(subset);
+          }
+      }
+      return result;
+  }
 }
